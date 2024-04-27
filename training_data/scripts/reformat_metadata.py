f56c9b80694d2af60840e353701bd09368b1dc07
henryjosephson <131294008+henryjosephson@users.noreply.github.com>
2024-04-27 12:40:39 -0500

    apparently we have to repeat the transcriptions twice? (&script to do it)

---
+++ training_data/scripts/reformat_metadata.py
@@ -0,0 +1,8 @@
+with open("../MyTTSDataset/metadata.csv","r") as f:
+    lines = f.readlines()
+
+with open("../MyTTSDataset/metadata.csv","w") as f:
+    for line in lines:
+        line = line.split("|")
+        line = [x.replace("\n","") for x in line] + [line[-1]]
+        f.write("|".join(line))
