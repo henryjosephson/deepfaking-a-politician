88d7f483bf3b76f052ffa0fb218a1b7d5318b5b6
henryjosephson <131294008+henryjosephson@users.noreply.github.com>
2024-05-06 14:46:43 -0500

    edited script

--- training_data/scripts/reformat_metadata.py
+++ training_data/scripts/reformat_metadata.py
@@ -4,5 +4,5 @@ with open("../MyTTSDataset/metadata.csv","r") as f:
 with open("../MyTTSDataset/metadata.csv","w") as f:
     for line in lines:
         line = line.split("|")
-        line = [x.replace("\n","") for x in line] + [line[-1]]
+        line = [x.replace("\n","").strip() for x in line] + [line[-1]]
         f.write("|".join(line))
